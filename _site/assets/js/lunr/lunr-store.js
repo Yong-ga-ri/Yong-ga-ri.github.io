08ae63add4519ba1273cf24976133c85c6f4b7ae
gilhan <rlfgks97@gmail.com>
2022-01-24 15:58:08 -0800

    <>

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,18 @@ var store = [{
         "title": "First Posting ",
         "excerpt":"첫 잔에 배부르지 말자   이제 시작하는 입장으로서 겁도 나고 “내가 과연 할 수 있을까?”라는 생각이 든다.   하지만 나는 할 수 있다고 자신한다. 앞으로의 내게 기대를 걸어본다.  ","categories": [],
         "tags": ["Blog"],
-        "url": "/firstPosting/",
+        "url": "/220122_firstPosting/",
+        "teaser": null
+      },{
+        "title": "Second Posting ",
+        "excerpt":"첫 잔에 배부르지 말자   이제 시작하는 입장으로서 겁도 나고 “내가 과연 할 수 있을까?”라는 생각이 든다.   하지만 나는 할 수 있다고 자신한다. 앞으로의 내게 기대를 걸어본다.  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/220123_practice/",
+        "teaser": null
+      },{
+        "title": "Figma",
+        "excerpt":"첫 잔에 배부르지 말자   이제 시작하는 입장으로서 겁도 나고 “내가 과연 할 수 있을까?”라는 생각이 든다.   하지만 나는 할 수 있다고 자신한다. 앞으로의 내게 기대를 걸어본다.  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/220124_Figma/",
         "teaser": null
       }]
